941859a021b670ffaa5a3a70b407558d9095f031
Shail Patel <shailpatel2@gmail.com>
2020-11-13 08:52:35 -0800

    Initial setup

---
+++ src/main/java/org/goahead/server/mapMyTripConfiguration.java
@@ -0,0 +1,10 @@
+package org.goahead.server;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class mapMyTripConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
