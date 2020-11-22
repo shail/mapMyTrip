ad9c457ae2ff4f62a6fee0aa1e489467d0eab88f
Shail Patel <shailpatel2@gmail.com>
2020-11-22 11:26:15 -0800

    Add more trips resource tests

--- src/test/java/org/goahead/server/resources/TripsResourceTest.java
+++ src/test/java/org/goahead/server/resources/TripsResourceTest.java
@@ -1,12 +1,16 @@
 package org.goahead.server.resources;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import io.dropwizard.testing.junit5.ResourceExtension;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import org.assertj.core.util.Lists;
 import org.goahead.server.api.LatLng;
 import org.goahead.server.api.TripRepresentation;
@@ -34,5 +38,39 @@ public class TripsResourceTest {
             .readEntity(TripRepresentation[].class);
     assertThat(trips.length).isEqualTo(1);
     assertThat(new Trip(trips[0])).isEqualTo(trip);
+  }
+
+  @Test
+  public void testGetTrip() {
+    final int id = 1;
+    when(tripsService.getTrip(eq(id))).thenReturn(trip);
+    TripRepresentation tripResult =
+        resources
+            .target(TRIPS_ENDPOINT + "/" + id)
+            .request()
+            .get(Response.class)
+            .readEntity(TripRepresentation.class);
+    assertThat(tripResult).isEqualTo(new TripRepresentation(trip));
+  }
+
+  @Test
+  public void testDeleteTrip() {
+    final int id = 1;
+    when(tripsService.deleteTrip(eq(id))).thenReturn(true);
+    Response deleteResponse =
+        resources.target(TRIPS_ENDPOINT + "/" + id).request().delete(Response.class);
+    assertThat(deleteResponse.getStatus()).isEqualTo(Status.OK.getStatusCode());
+  }
+
+  @Test
+  public void testCreateTrip() {
+    when(tripsService.createTrip(new TripRepresentation(trip))).thenReturn(trip);
+    TripRepresentation createdTrip =
+        resources
+            .target(TRIPS_ENDPOINT)
+            .request()
+            .post(Entity.entity(new TripRepresentation(trip), MediaType.APPLICATION_JSON_TYPE))
+            .readEntity(TripRepresentation.class);
+    assertThat(createdTrip).isEqualTo(new TripRepresentation(trip));
   }
 }
